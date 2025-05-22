b72a0b248919b737504cb6fbb183f9e5d444c494
APAC\POH5HC <Oai.PhungGia@vn.bosch.com>
2025-05-22 16:18:52 +0700

    update noti for ios

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+if (typeof DeviceOrientationEvent.requestPermission === "function") {
+  // iOS 13+ requires permission
+  document.getElementById("permissionButton").addEventListener("click", () => {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          window.addEventListener(
+            "deviceorientation",
+            (event) => {
+              const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+              const leftToRight = event.gamma; // gamma: left to right
+              const frontToBack = event.beta; // beta: front back
+              console.log(
+                `Rotation: ${rotateDegrees}, Left to Right: ${leftToRight}, Front to Back: ${frontToBack}`
+              );
+
+              document.getElementById("test-section").innerHTML = `
+      <p>Rotation: ${rotateDegrees}</p>
+      <p>Left to Right: ${leftToRight}</p>
+      <p>Front to Back: ${frontToBack}</p>
+    `;
+            },
+            true
+          );
+        } else {
+          console.log("Permission denied for Device Orientation.");
+          document.getElementById("test-section").innerHTML = `
+        <p>Permission denied for Device Orientation.</p>
+    `;
+        }
+      })
+      .catch((error) => {
+        console.error("Error requesting permission:", error);
+      });
+  });
+}
 if (window.DeviceOrientationEvent) {
   addEventListener("deviceorientation", (event) => {
     const rotateDegrees = event.alpha; // alpha: rotation around z-axis
