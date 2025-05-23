81f697fafc51d91b339e1d3e5faa4b9e0f6e5839
APAC\POH5HC <Oai.PhungGia@vn.bosch.com>
2025-05-23 15:41:13 +0700

    update card

--- index.js
+++ index.js
@@ -34,25 +34,66 @@ if (typeof DeviceOrientationEvent.requestPermission === "function") {
       });
   });
 }
-if (window.DeviceOrientationEvent) {
-  addEventListener("deviceorientation", (event) => {
-    const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-    const leftToRight = event.gamma; // gamma: left to right
-    const frontToBack = event.beta; // beta: front back
+// if (window.DeviceOrientationEvent) {
+//   addEventListener("deviceorientation", (event) => {
+//     const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+//     const leftToRight = event.gamma; // gamma: left to right
+//     const frontToBack = event.beta; // beta: front back
 
-    console.log(
-      `Rotation: ${rotateDegrees}, Left to Right: ${leftToRight}, Front to Back: ${frontToBack}`
-    );
+//     console.log(
+//       `Rotation: ${rotateDegrees}, Left to Right: ${leftToRight}, Front to Back: ${frontToBack}`
+//     );
 
-    document.getElementById("test-section").innerHTML = `
-      <p>Rotation: ${rotateDegrees}</p>
-      <p>Left to Right: ${leftToRight}</p>
-      <p>Front to Back: ${frontToBack}</p>
-    `;
-  });
-} else {
-  console.log("Device Orientation API not supported");
-  document.getElementById("test-section").innerHTML = `
-        <p>Device Orientation API not supported</p>
-    `;
-}
+//     document.getElementById("test-section").innerHTML = `
+//       <p>Rotation: ${rotateDegrees}</p>
+//       <p>Left to Right: ${leftToRight}</p>
+//       <p>Front to Back: ${frontToBack}</p>
+//     `;
+//   });
+// } else {
+//   console.log("Device Orientation API not supported");
+//   document.getElementById("test-section").innerHTML = `
+//         <p>Device Orientation API not supported</p>
+//     `;
+// }
+// ================ Card Animation ================
+// DOM Element selections
+const cardWrapper = document.querySelector(".cardWrapper");
+const card = document.querySelector(".card");
+const highlight = document.querySelector(".highlight");
+
+// highest values for angle
+const mostX = 10; // 10 or -10
+const mostY = 10; // 10 or -10
+
+cardWrapper.addEventListener("mousemove", (e) => {
+  // remove transition
+  card.style.transition = "none";
+  highlight.style.transition = "none";
+
+  const x = e.offsetX;
+  const y = e.offsetY;
+  const { width, height } = cardWrapper.getBoundingClientRect();
+  const halfWidth = width / 2;
+  const halfHeight = height / 2;
+
+  // calculate angle
+  const rotationY = ((x - halfWidth) / halfWidth) * mostX;
+  const rotationX = ((y - halfHeight) / halfHeight) * mostY;
+
+  // set rotation
+  card.style.transform = `rotateY(${rotationY}deg) rotateX(${rotationX}deg)`;
+  highlight.style.left = `${(rotationY / mostX) * 60 * -1}%`;
+  highlight.style.top = `${(rotationX / mostY) * 60 * -1}%`;
+});
+
+cardWrapper.addEventListener("mouseleave", () => {
+  // add transition back
+  card.style.transition = "transform 0.5s ease-in-out";
+  card.style.transform = `rotateY(0) rotateX(0)`;
+  highlight.style.transition = "left 0.5s ease-in-out, top 0.5s ease-in-out";
+
+  // add default position back to highlight
+  highlight.style.left = `16%`;
+  highlight.style.top = `10%`;
+});
